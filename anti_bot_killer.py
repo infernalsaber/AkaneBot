a10c219ffbe9444b0fba8b8093f6dcba4ed2bdec
infernalsaber <72517486+infernalsaber@users.noreply.github.com>
2023-07-12 21:42:09 +0000

    Fixed the updating/bot uptime keeping script

--- anti_bot_killer.py
+++ anti_bot_killer.py
@@ -1,18 +1,35 @@
-import asyncio
+import time
+import subprocess
 import os
 
+check = "ps aux | grep bot.py | grep -v 'grep' | awk '{print $2}'"
+
 while True:
-    if os.path.exists("ded.txt"):
-        os.remove("ded.txt")
-        break
+    try:
+        if os.path.exists("ded.txt"):
+            os.remove("ded.txt")
+            break
 
-    if os.system("ps aux | grep bot.py | grep -v 'grep' | awk '{print $2}'") == 0:
-        print("Starting bot.")
+        process = subprocess.Popen(check, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        if not int.from_bytes(stdout, byteorder='big'):
+            print("Starting bot.")
 
-        if os.name == "nt":
-            os.system("nohup python -OO bot.py &")
+            if os.name == "nt":
+                os.system("nohup python -OO bot.py >> logs/output.log 2>&1 &")
+            else:
+                os.system("nohup python3 -OO bot.py >> logs/output.log 2>&1 &")
+
         else:
-            os.system("nohup python3 -OO bot.py &")
+            print("Bot is already up")
+            time.sleep(10)
+    except Exception as e:
+        print(e)
+# print(type(os.system("ps aux | grep bot.py | grep -v 'grep' | awk '{print $2}'")))
+# print(int(os.system("ps aux | grep bot.py | grep -v 'grep' | awk '{print $2}'")) == 0)
 
-    else:
-        asyncio.sleep(10)
+
+
+# print(stdout, type(stdout))
+# x = int.from_bytes(stdout, byteorder='big')
+# print(x, x == 0)
